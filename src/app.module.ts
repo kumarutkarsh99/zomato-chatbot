d073a758e0059841a64adb9e53f8be64b3572e9d
Kumar Utkarsh <kumar.cs01032@gmail.com>
2025-08-15 20:15:15 +0530

    removed errors

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,8 @@ import { DatabaseService } from './database/database.service';
 import { RestaurantModule } from './restaurant/restaurant.module';
 import { MenusModule } from './menus/menus.module';
 import { ChatbotModule } from './chatbot/chatbot.module';
-import { SupabaseModule } from './supabase/supabase.module';
-
 @Module({
-  imports: [RestaurantModule, MenusModule, ChatbotModule, SupabaseModule],
+  imports: [RestaurantModule, MenusModule, ChatbotModule],
   controllers: [AppController],
   providers: [AppService, DatabaseService],
   exports: [DatabaseService],
